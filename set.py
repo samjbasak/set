c2418f8788086895b7453cc0bc4eaa74f6953ff9
Sam Headleand <samheadleand@hotmail.com>
2013-03-17 12:52:37 +0000

    Describe card types in a comment

--- set.py
+++ set.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
 print("Hello, world!")
+
+# (Number,Colour,Shape,Fill)
+# Number	1,2 or 3
+# Colour	G - Green
+#		R - Red
+#		P - Purple
+# Shape		D - Diamond
+#		O - Oval
+#		S - Squiggle
+# Fill		E - Empty
+#		H - Hatched
+#		F - Filled		
